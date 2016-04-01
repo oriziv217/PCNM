5c89f77349bc38e16da6baaac6424c47261b2175
ori ziv <ori ziv@IT-WUD.elcam.co.il>
2016-04-01 21:19:16 +0300

    Workstation search screen half done1

---
+++ PCNM/src/Entities/Workstation.java
@@ -0,0 +1,111 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package Entities;
+
+import java.io.Serializable;
+
+/**
+ *This class represents a record in Workstations table
+ * @author Sivan Yehuda
+ * @author Ori Ziv
+ */
+public class Workstation implements Serializable{
+    private int ID;
+    private String name;
+    private String description;
+    private double importanceFactor;
+    private Status status;
+    private WSType type;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscription() {
+        return description;
+    }
+
+    public void setDiscription(String discription) {
+        this.description = discription;
+    }
+
+    public double getImportanceFactor() {
+        return importanceFactor;
+    }
+
+    public void setImportanceFactor(double importanceFactor) {
+        this.importanceFactor = importanceFactor;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Workstation() {
+    }
+
+    public Workstation(String name, String discription, double importanceFactor, Status status) {
+        this.name = name;
+        this.description = discription;
+        this.importanceFactor = importanceFactor;
+        this.status = status;
+    }
+
+    public Workstation(String name, String discription, double importanceFactor, Status status, WSType type) {
+        this.name = name;
+        this.description = discription;
+        this.importanceFactor = importanceFactor;
+        this.status = status;
+        this.type = type;
+    }
+
+    public Workstation(int ID, String name, String discription, double importanceFactor, Status status, WSType type) {
+        this.ID = ID;
+        this.name = name;
+        this.description = discription;
+        this.importanceFactor = importanceFactor;
+        this.status = status;
+        this.type = type;
+    }
+
+    public String toString() {
+        String sts = "Error";
+        
+        switch (this.status) {
+            case ENABLE:
+                sts = "Enabled";
+                break;
+            case DISABLE:
+                sts = "Disabled";
+                break;
+            case SUSPENDED:
+                sts = "Suspended";
+                break;
+        }
+        return String.valueOf(ID) + ","
+                + name + ","
+                + description + ","
+                + String.valueOf(importanceFactor) + ","
+                + sts + ","
+                + type.toString();
+    }
+}
+
